80cf8d686935220c54d8741e26102ca247e0e013
hardikpnsp <hardikpnsp@gmail.com>
2019-02-25 17:59:15 +0530

    added second player !

---
+++ Scenes/level1.gd
@@ -0,0 +1,31 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+onready var scorep1 = get_node("hud/ScoreP1")
+onready var scorep2 = get_node("hud/ScoreP2")
+#onready var player1 = get_node("player1")
+onready var player1 = load("res://Scenes/player.tscn")
+onready var player2 = load("res://Scenes/player.tscn")
+onready var p1 = player1.instance()
+onready var p2 = player2.instance()
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	self.add_child(p1)
+	p1.move_to(Vector2(50, -200))
+	p1.setKey("wasd_up", "wasd_right", "wasd_left")
+	self.add_child(p2)
+	p2.move_to(Vector2(200, -200))
+	scorep1.add_text("Potato: 5")
+	scorep2.push_align(HALIGN_RIGHT)
+	scorep2.add_text("Tomato: 5")
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	var p = p1.get_pos()
+	if p.y > 600:
+		scorep1.clear()
+		scorep1.add_text("dead")
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -11,21 +11,31 @@ const MAX_SPEED = 500
 
 var acc = Vector2()
 var vel = Vector2()
-
+var score = 5
+var jump = "ui_up"
+var right = "ui_right"
+var left = "ui_left"
 
 func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
 
+func setKey(j,r,l):
+	jump = j
+	print("%s", jump)
+	right = r
+	left = l
+	
+
 func _input(event):
-	if event.is_action_pressed("ui_up") and ground_ray.is_colliding():
+	if event.is_action_pressed(jump) and ground_ray.is_colliding():
 		vel.y = JUMP_SPEED
-	if event.is_action_released("ui_up"):
+	if event.is_action_released(jump):
 		vel.y = clamp(vel.y, MIN_JUMP_SPEED, vel.y)
 
 func _fixed_process(delta):
 	acc.y = GRAVITY
-	acc.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	acc.x = Input.is_action_pressed(right) - Input.is_action_pressed(left)
 	acc.x *= ACCELERATION
 	if acc.x == 0:
 		acc.x = vel.x * FRICTION * delta
